54af9409adb932ab9ddba9b50093f18c7a51bac1
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-12-16 10:58:08 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -8,15 +8,15 @@ namespace MetodosConstrutores
         static void Main(string[] args)
         {
             Produto produto1 = new Produto();
-            produto1.Code = 51; 
+            produto1.Code = 7; 
             produto1.Name = "Fanta Laranja"; 
             produto1.Description = "Refrigerante sabor laranja";
             Console.WriteLine($"Código do produto: {produto1.Code} - Nome do Produto: {produto1.Name} - Descrição do produto: {produto1.Description}");
 
-            Produto produto2 = new Produto(0001);
+            Produto produto2 = new Produto(1);
             Console.WriteLine($"Código do Produto: {produto2.Code}");
 
-            Produto produto3 = new Produto(0001,"Café Pilão","Café Tradicional 500g",5000);
+            Produto produto3 = new Produto(1,"Café Pilão","Café Tradicional 500g",200);
             Console.WriteLine($"Código do produto: {produto3.Code} - Nome do produto: {produto3.Name} - Descrição do produto: {produto3.Description} - Estoque do produto: {produto3.Stock}");
         }
     }
